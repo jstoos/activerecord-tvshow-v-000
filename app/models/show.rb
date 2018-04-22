f123d7375f4ee3457f9f46785a3e46c6d926ae5c
Jennifer Stoos <jstoos@bitlaw.com>
2018-04-22 16:25:33 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,8 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    x = self.highest_rating
-    Show.where(rating: x)
+    Show.where("rating = ?", self.highest_rating)
   end
 
 end
